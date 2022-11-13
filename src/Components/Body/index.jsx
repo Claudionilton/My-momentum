e16d148dfc018dc91ab5b71109fb0349143b4741
Claudionilton <claudionilton.junior@liferay.com>
2022-11-13 18:26:00 -0300

    Add Hour Painel

--- src/Components/Body/index.jsx
+++ src/Components/Body/index.jsx
@@ -1,14 +1,33 @@
-import { useEffect } from "react";
+const Body = () => {
+  setInterval(() => {
+    let date = new Date();
+    let dateHour = date.getHours();
+    let dateMin = date.getMinutes();
+    let dateSec = date.getSeconds();
 
-const Body = (props) => {
-  const time = new Date().toLocaleTimeString();
-  const date = new Date().toLocaleDateString();
+    hour.innerText = `${formatTime(dateHour)}`;
+    min.innerText = `${formatTime(dateMin)}`;
+    sec.innerText = `${formatTime(dateSec)}`;
+  }, 1000);
 
+  function formatTime(time) {
+    return time < 10 ? "0" + time : time;
+  }
   return (
-    <div className="h-screen w-screen p-6 bg-purple-400 rounded-sm ">
-      {props.children}
-      <div className="relative h-32 w-32">{time}</div>
-      <div className="relative h-50 w-50">{date}</div>
+    <div className=" text-blue-600 h-screen w-screen p-6  rounded-sm relative">
+      <img
+        src="https://picsum.photos/200/300"
+        alt="image"
+        height="536"
+        width="354"
+      />
+      <div className="flex justify-center text-2xl absolute inset-0 place-items-center">
+        <span id="hour">00</span>
+        <span>:</span>
+        <span id="min">00</span>
+        <span>:</span>
+        <span id="sec">00</span>
+      </div>
     </div>
   );
 };
